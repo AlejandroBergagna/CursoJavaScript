cb32a6af7b946b891bed570012190b7301ef4b5d
Alejandro <alebergagna0@gmail.com>
2022-08-17 11:08:32 -0300

    primer ejercicio JavaScript

---
+++ javascript/javascript.js
@@ -0,0 +1,18 @@
+//EJERCICIO PROMEDIO DE NOTAS
+
+let nombre = prompt("Ingrese nombre del alumno: ");
+console.log(nombre);
+
+let nota1 = prompt("Ingrese nota 1: ");
+
+let nota2 = prompt("Ingrese nota 2: ");
+
+let nota3 = prompt("Ingrese nota 3: ");
+
+
+let promedio = parseFloat((parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / 3);
+
+console.log(promedio);
+
+alert(`El promedio de ${nombre} es ${promedio}`);
+
